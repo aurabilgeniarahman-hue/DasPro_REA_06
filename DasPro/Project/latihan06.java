e943701126387777df704faf711cec90d1aa2737
aurabilgeniarahman-hue <aurabilgeniarahman@gmail.com>
2026-09-02 21:53:51 +0700

    Delete DasPro/Project/latihan06.java

--- DasPro/Project/latihan06.java
+++
@@ -1,8 +0,0 @@
-public class latihan06 {
-    public static void main(String[] args ){
-        System.out.println("Nama saya reaa");
-        System.out.print("\nBelajar Java itu mudah");
-        //System.out.println("Semakin sering mencoba, semakin jago");
-
-    }
-}
